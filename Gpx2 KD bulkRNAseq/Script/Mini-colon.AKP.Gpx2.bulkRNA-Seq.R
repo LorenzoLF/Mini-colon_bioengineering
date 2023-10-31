d7c30bb594a1944165ec2177d13083c387d589bb
L. Francisco Lorenzo-Martín <40798046+LorenzoLF@users.noreply.github.com>
2023-10-31 12:28:13 +0100

    Add files via upload

---
+++ Gpx2 KD bulkRNAseq/Script/Mini-colon.AKP.Gpx2.bulkRNA-Seq.R
@@ -0,0 +1,116 @@
+############## Spatiotemporally resolved ex vivo colorectal cancer development in engineered mini-colons##############
+############## Gpx2 knockdown in colon organoids before and after blue light-mediated AKP oncogenic recombination  ##############
+
+
+
+##### ------------------------------------------- 1. Setup -------------------------------------------
+
+##Packages
+library(edgeR)
+library(limma)
+library(gplots)
+library(heatmap3)
+library(org.Mm.eg.db)
+library(EnhancedVolcano)
+
+##bulk RNA-Seq data (available in Resources folder)
+countdata <- read.table("countdata.txt", sep = "\t", header = T)
+#Sample info
+sampleinfo <- read.table("sampleinfo.txt", sep ="\t", header = T)
+colnames(countdata) <- sampleinfo$Name
+
+
+
+##### ------------------------------------------- 2. Gene filtering -------------------------------------------
+
+##Gene filtering
+myCPM <- cpm(countdata)
+thresh <- myCPM > 1
+keep <- rowSums(thresh) >= 3
+counts.keep <- countdata[keep,]
+
+##Conversion to DGEList object
+dgeObj <- DGEList(counts.keep, samples = sampleinfo)
+
+
+
+##### ------------------------------------------- 3. Normalization -------------------------------------------
+
+##Count normalization with TMM
+dgeObj <- calcNormFactors(dgeObj, method = "TMM")
+#log2 counts per million
+logcounts <- cpm(dgeObj,log=TRUE)
+
+##Sample distributions
+boxplot(logcounts, xlab="", ylab="Log2 counts per million",las=2)
+
+##Data exploration
+dc = dist(t(logcounts), method = "euclidean")
+hc = hclust(dc, method="complete")
+plot(hc)
+
+
+
+##### ------------------------------------------- 4. Differential expression -------------------------------------------
+
+##Design matrix creation
+group <- dgeObj$samples$Group
+group <- factor(group)
+design <- model.matrix(~ 0 + group)
+#Make the column names of the design matrix nicer
+colnames(design) <- gsub("group", "", colnames(design))
+
+##Voom-transformation of the data
+v <- voom(dgeObj, design, plot = TRUE)
+
+##Linear model fit
+fit <- lmFit(v)
+
+##Contrast matrix creation
+contrast.matrix = makeContrasts(
+  shGpx2_FT_2w_vs_dR4_FT_2w = AKP_shGpx2_FT_2w - dR4_FT_2w,
+  shGpx2_vs_dR4 = AKP_shGpx2 - dR4,
+  levels = design)
+
+##Contrast calculation
+fit.cont <- contrasts.fit(fit, contrast.matrix)
+
+##Empirical Bayes
+res.limma <- eBayes(fit.cont)
+
+##Gene annotation
+id.symbol <- mapIds(org.Mm.eg.db, keys = rownames(fit.cont), keytype = "ENSEMBL", column="SYMBOL")
+res.limma$genes <- id.symbol
+v$genes <- id.symbol
+
+##Results
+#For multiple comparisons in the contrast matrix, select comparison (choose between 'coef = "shGpx2_vs_dR4"' (before oncogenic recombination) and 'coef = "shGpx2_FT_2w_vs_dR4_FT_2w"' (after oncogenic recombination):
+results <- topTable(res.limma, p.value=0.05, adjust.method="BH", sort.by="p", number=nrow(res.limma), coef = "shGpx2_FT_2w_vs_dR4_FT_2w")
+head(results)
+
+##Heatmap visualization of genes of interest
+ensid <- mapIds(org.Mm.eg.db, keys = c("Sox9","Cd44","Lgr5"), keytype = "SYMBOL", column="ENSEMBL")
+expM <- logcounts[ensid,]
+png(filename = "Heatmap.png", width = 900, height = 600)
+heatmap3(expM, balanceColor = T, labRow = id.symbol[ensid], margins = c(30,5), Colv=NA, Rowv = NA, cexRow = 1.5, cexCol = 1.5)
+dev.off()
+#Check the file "Heatmap.png" in your working directory to see the heatmap
+
+#Volcano visualization of differentially-expressed genes
+results.full <- topTable(res.limma, adjust.method="BH", sort.by="p", number=Inf, coef = "shGpx2_FT_2w_vs_dR4_FT_2w")
+png(filename = "Volcano.png", width = 700, height = 700)
+EnhancedVolcano(results.full,
+                lab = results.full$ID,
+                x = "logFC",
+                y = "adj.P.Val",
+                pCutoff = 0.05,
+                FCcutoff = 1,
+                ylab = bquote(~-Log[10] ~ italic(Q)),
+                title = 'shGpx2_AKP vs Control_AKP',
+                labSize = 4,
+                drawConnectors = TRUE, typeConnectors = "open", arrowheads = FALSE, maxoverlapsConnectors = Inf,
+                selectLab = "Gpx2",
+                gridlines.major = FALSE,
+                gridlines.minor = FALSE)
+dev.off()
+#Check the file "Volcano.png" in your working directory to see the volcano plot
